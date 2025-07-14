e1031818c6fca98b9e00c81db7a368e4e82f5d96
Dhanashree2023 <Prachi123451@gmail.com>
2025-07-14 19:13:49 +0530

    First

---
+++ src/com/TEST.java
@@ -0,0 +1,14 @@
+package com;
+
+public class TEST {
+
+	public static void main(String[] args) {
+
+          System.out.println("A");
+          System.out.println("B");
+          System.out.println("C");
+          System.out.println("D");
+
+	}
+
+}
